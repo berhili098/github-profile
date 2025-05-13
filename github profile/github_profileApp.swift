9c9e09ce351fb17b058f951f7ffa3d87db6c2d4d
oussama berhili <oussamaberhili190@gmail.com>
2025-05-13 16:10:19 +0100

    Initial Commit

---
+++ github profile/github_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_profileApp.swift
+//  github profile
+//
+//  Created by oussama berhili on 13/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct github_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ github profileTests/github_profileTests.swift
@@ -0,0 +1,16 @@
+//
+//  github_profileTests.swift
+//  github profileTests
+//
+//  Created by oussama berhili on 13/5/2025.
+//
+
+import Testing
+
+struct github_profileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
